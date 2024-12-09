e9b7dcabc519d173a64b9faba15ddb6697d20998
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2024-12-09 10:47:23 +0100

    fix gha for CD

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod configuration;
 mod database;
 mod logger;
+#[cfg(test)]
 mod test;
 
 use crate::configuration::Configuration;
@@ -12,13 +13,35 @@ use std::time::Instant;
 fn main() {
     let start_time: Instant = Instant::now();
     let mut logger: Logger = Logger::new();
-    let config: Configuration = Configuration::get_from_args(); // Get the configuration object
-    let conn: Connection = open_connection(config.get_db_path(), &mut logger); // Open the connection to the database
-    let start_size: u64 = config.get_size_of_database();
+    let config: Configuration = match Configuration::get_from_args() {
+        Ok(c) => c,
+        Err(e) => {
+            logger.log_and_print(&format!("Error getting configuration: {:?}", e));
+            return;
+        }
+    };
+
+    let conn: Connection = match open_connection(config.get_db_path(), &mut logger) {
+        Ok(c) => c,
+        Err(e) => {
+            logger.log_and_print(&format!("Error opening connection to database: {:?}", e));
+            return;
+        }
+    };
+
+    let start_bytes_size: u64 = config.get_size_of_database().unwrap_or_default();
 
     println!("Optimizing database...");
-    println!("Size at start {:?} bytes", start_size);
-    process_db_cleaning(&conn, &mut logger); // Process the database cleaning
-    print_report(start_time, start_size, &config, &mut logger);
+    println!("Size at start {:?} bytes", start_bytes_size);
+
+    match process_db_cleaning(&conn, &mut logger) {
+        Ok(_) => (),
+        Err(e) => {
+            logger.log_and_print(&format!("Error processing the cleaning: {:?}", e));
+            return;
+        }
+    }
+
+    print_report(start_time, start_bytes_size, &config, &mut logger);
     return;
 }
